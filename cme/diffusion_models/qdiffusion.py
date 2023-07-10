d11976523f3320bd7e1e87926951857a6bcc2e65
Ritesh K. Malaiya <ritesh.malaiya@gmail.com>
2023-07-10 16:03:29 -0500

    fixed bug related to varaince in qdiffusion

--- cme/diffusion_models/qdiffusion.py
+++ cme/diffusion_models/qdiffusion.py
@@ -73,9 +73,9 @@ def q_diffusion(a_i, v_i, a_p, v_p, t_er, grad=False):
         1 + (V_RT_kj / np.power(E_RT_kj,2))
     )
 
-    sigma_kj = np.log(
+    sigma_kj = np.sqrt(np.log(
         1 + (V_RT_kj / np.power(E_RT_kj,2))
-    )
+    ))
     return mu_kj, sigma_kj, p_jk
 
 def get_state(df_posterior, K, J):
