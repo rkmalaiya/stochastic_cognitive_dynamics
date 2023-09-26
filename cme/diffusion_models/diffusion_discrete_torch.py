6b25366d38cd93e4e704f9709879dfa32ceb103d
Ritesh Malaiya <ritesh.malaiya@gmail.com>
2023-09-26 17:03:59 -0500

    test cases passed

--- cme/diffusion_models/diffusion_discrete_torch.py
+++ cme/diffusion_models/diffusion_discrete_torch.py
@@ -5,21 +5,28 @@ import pandas as pd
 import pyro as npy
 import pyro.distributions as dist
 from pyro.infer import MCMC, NUTS
+import numpy as n
 #npy.set_host_device_count(4)
 
-def _buildK(n_states, mu, sigma): 
+def _buildK(n_states, mu, sigma, n_part): 
 # m = number of states  
 # a = off diag left  
 # b = diag  
 # c = off diag right
-    n_part = np.as_tensor(mu).shape[0]
+    mu = np.as_tensor(mu)
+    #n_part = np.as_tensor(mu).shape[0]
     K = np.zeros((n_part, 1, n_states,n_states)) # participants, trials, transition states
 
     for j in range(n_part):
+        if (len(mu.shape) == 0):
+            mu_j = mu
+        else:
+            mu_j = mu[j]
+
         mk = np.ones((n_states,1))
         b = -sigma*mk
-        a1 = 0.5 * (sigma-mu[j])*mk
-        a2 = 0.5 * (sigma+mu[j])*mk
+        a1 = 0.5 * (sigma-mu_j)*mk
+        a2 = 0.5 * (sigma+mu_j)*mk
     
         for i in range(1, n_states-1):       
             #K = K.at[j,0,i, [i-1,i,i+1]].set([a1[i][0], b[i][0], a2[i][0]])
@@ -41,12 +48,12 @@ def _get_initial_state(n_states, start_width):
             Mr[i,i] = 1
     return s_0.double(), Mr.double()
 
-def likelihood(n_states, start_width, mu, sigma,rt,s_0, Mr):
-    K= _buildK(n_states, mu=mu, sigma=sigma)
+def likelihood(n_states, start_width, mu, sigma,rt,s_0, Mr, n_part):
+    K= _buildK(n_states, mu=mu, sigma=sigma, n_part=n_part)
     Mid = int((n_states+1)/2)
     mv = np.arange(-(Mid-1),(Mid)).double()
     rt = rt.unsqueeze(2).unsqueeze(3)
-    phi=ln.matrix_exp(rt) # rt:I,J,1,1; K:I,J,m,m
+    #phi=ln.matrix_exp(rt) # rt:I,J,1,1; K:I,J,m,m
     A = np.mul(rt,K)
     phi=ln.matrix_exp(A) # rt:I,J,1,1; K:I,J,m,m
     Pt = np.matmul(phi, s_0)
@@ -55,7 +62,7 @@ def likelihood(n_states, start_width, mu, sigma,rt,s_0, Mr):
 
     return Pt, Mc, Pcorrect.sum() #likelihood hence adding up over all responses.
 
-def perform_walk(n_states, start_width, mu, sigma,timesteps=1.5, delta=0.01):
+def perform_walk(n_states, start_width, mu, sigma, n_part, timesteps=1.5, delta=0.01):
 
     avg_conf = [];  
     state_prob = []
@@ -64,7 +71,7 @@ def perform_walk(n_states, start_width, mu, sigma,timesteps=1.5, delta=0.01):
 
     for rt in list(np.arange(0.1,timesteps,step=delta)):
         
-        Pt, Mc, Pcorrect = likelihood(n_states, start_width, mu, sigma,np.as_tensor([[rt]]),s_0, Mr)
+        Pt, Mc, Pcorrect = likelihood(n_states, start_width, mu, sigma,np.as_tensor([[rt]]),s_0, Mr, n_part)
 
         state_prob.append(Pt)
         correct_prob.append(Pcorrect)
@@ -88,39 +95,38 @@ def perform_walk(n_states, start_width, mu, sigma,timesteps=1.5, delta=0.01):
 
     return df_st, df_avg_conf, df_avg_corr
 
-def model(n_states, start_width, rt, s_0, Mr):
+def model(n_states, start_width, rt, s_0, Mr, n_part):
     I,J = rt.shape
-    #with npy.plate('I', I):
-        
-    mu =  npy.sample(f"mu", dist.Normal(0,5),sample_shape=(I,))
-    _,lkl ,_ = likelihood(n_states, start_width, mu, 1, rt, s_0, Mr)
+    with npy.plate('I', I):
+        mu =  npy.sample(f"mu", dist.Normal(0,5))
+        _,lkl ,_ = likelihood(n_states, start_width, mu, 1, rt, s_0, Mr, n_part)
     
-    npy.factor(f"likelihood", lkl)
+        npy.factor(f"likelihood", lkl)
 
-def sample_posterior_params(DT, X, num_warmup=100, samples_n=500, n_states=7, start_width=1):
+def sample_posterior_params(DT, X, n_part, num_warmup=100, samples_n=500, n_states=7, start_width=1):
 
     s_0, Mr = _get_initial_state(n_states, start_width)
 
     kernel = NUTS(model)
-    mcmc_chain = MCMC(kernel, num_warmup=num_warmup, num_samples=samples_n, num_chains=4)
-    mcmc_chain.run(n_states, start_width, DT, s_0, Mr)
+    mcmc_chain = MCMC(kernel, warmup_steps =num_warmup, num_samples=samples_n, num_chains=4)
+    mcmc_chain.run(n_states, start_width, DT, s_0, Mr, n_part)
     return mcmc_chain
 
 #%%
 if __name__ == "__main__":
 
-    print(_buildK(7, mu=[1.5], sigma=1))
+    print(_buildK(7, mu=[1.5], sigma=1, n_part=1))
     print("*****************")
-    print(_buildK(7, mu=[0.5,1.5], sigma=1))
+    print(_buildK(7, mu=[0.5,1.5], sigma=1,n_part=1))
     print("*****************")
 
-    K = _buildK(7, mu=[1], sigma=1)
+    K = _buildK(7, mu=[1], sigma=1, n_part=1)
     print("test 1", K.shape)
-    K = _buildK(7, mu=np.as_tensor([1,2,0.5]), sigma=1)
+    K = _buildK(7, mu=np.as_tensor([1,2,0.5]), sigma=1, n_part=1)
     print("test 2", K.shape)
  
 if __name__ == "__main__":
-    df_st, df_avg_conf, df_avg_corr = perform_walk(n_states=7, start_width=3, mu=[0.5], sigma=2,timesteps=20, delta=0.1)
+    df_st, df_avg_conf, df_avg_corr = perform_walk(n_states=7, start_width=3, mu=[0.5], sigma=2, n_part=1, timesteps=20, delta=0.1)
     print("test 3")
     #df_avg_conf.iloc[:,1].plot(xlabel="Number of Steps", ylabel="Average Confidence")
     #df_avg_corr.iloc[:,1].plot()
@@ -140,39 +146,42 @@ if __name__ == "__main__":
 #%%  
 import numpy as n
 if __name__ == "__main__":
-    rotation_RT = pd.read_csv("examples/data/final_project_rt.csv")
-    rotation_RT_n = rotation_RT.loc[~rotation_RT.isna().any(axis=1),:].to_numpy()
-
+    rotation_RT = n.loadtxt("../../examples/data/final_project_rt.csv",delimiter=",", skiprows=1)
+    #rotation_RT_n = rotation_RT.loc[~rotation_RT.isna().any(axis=1),:].to_numpy()
+    rotation_RT_n = np.from_numpy(rotation_RT)
     s_0, Mr = _get_initial_state(7, 1)
     Pcorrect_arr = []
     for mu in np.linspace(0.1,1.5,50):
-        Pt, Mc, Pcorrect = likelihood(7, 1, [mu], 1 , np.from_numpy(n.ones_like(rotation_RT_n)), s_0, Mr)
+        Pt, Mc, Pcorrect = likelihood(7, 1, [mu], 1 , np.ones((14,10)), s_0, Mr, 1)
         Pcorrect_arr.append(Pcorrect)
 
     print("Pcorrect", np.asarray(Pcorrect_arr))
     print("Mc", Mc.shape)
     print("Pt", Pt.shape)
-
+    print("test 4")
 
     s_0, Mr = _get_initial_state(101, 11)
     Pcorrect_arr = []
     for mu in np.linspace(0.1,1.5,50):
-        Pt, Mc, Pcorrect = likelihood(101, 11, [mu], 1,rotation_RT_n, s_0, Mr)
+        Pt, Mc, Pcorrect = likelihood(101, 11, [mu], 1,rotation_RT_n, s_0, Mr, 1)
         Pcorrect_arr.append(Pcorrect)
 
     print("Pcorrect", np.asarray(Pcorrect_arr))
     print("Mc", Mc.shape)
     print("Pt", Pt.shape)
+    print("test 5")
+
 
     s_0, Mr = _get_initial_state(103, 11)
-    Pt, Mc, Pcorrect = likelihood(103, 11, np.linspace(0.1,1.5,rotation_RT_n.shape[0]), 1,rotation_RT_n, s_0, Mr)
+    Pt, Mc, Pcorrect = likelihood(103, 11, np.linspace(0.1,1.5,rotation_RT_n.shape[0]), 1,rotation_RT_n, s_0, Mr, rotation_RT_n.shape[0])
     print("Pcorrect", Pcorrect)
     print("Mc", Mc.shape)
     print("Pt", Pt.shape)
-    #plt.show()
+    print("test 6")
 
-    mcmc_chain = sample_posterior_params(rotation_RT_n, None, num_warmup=10, samples_n=50, n_states=7, start_width=3)
+    mcmc_chain = sample_posterior_params(rotation_RT_n, None, rotation_RT_n.shape[0] ,num_warmup=10, samples_n=50, n_states=7, start_width=3)
     mcmc_chain.print_summary()
+    print("test 7")
 
 
 
