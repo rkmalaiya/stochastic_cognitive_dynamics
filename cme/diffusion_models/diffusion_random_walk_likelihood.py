09847f91264facb89e1d0a5712e8400dcd59c8b8
Ritesh Malaiya <ritesh.malaiya@gmail.com>
2023-11-07 22:54:48 -0600

    tested for multiple dimensions

--- cme/diffusion_models/diffusion_random_walk_likelihood.py
+++ cme/diffusion_models/diffusion_random_walk_likelihood.py
@@ -1,90 +1,139 @@
 #%%
-import pytensor.tensor as at
-import pymc as pm
+import jax.numpy as at
 import cme.utils.common_logging as cl
-from joblib import Parallel, delayed
 import numpy as np
-import pytensor.tensor.subtensor as st
 import arviz as az
+import numpyro as ro
+import numpyro.distributions as dist
+from numpyro.infer import Predictive
+from jax import random
+from numpyro.infer import MCMC, NUTS
 
 log = cl.get_logger("random-walk")
 
 
-def _get_n_states(alpha, theta, tau, sigma):
+def get_n_states(alpha, theta, tau, sigma):
     delta_state = alpha * sigma * at.sqrt(tau)
-    n_states = round(theta/delta_state) #2 * round(theta/delta_state) + 1
-    return n_states
+    n_states = at.round(theta/delta_state) #2 * round(theta/delta_state) + 1
+    return n_states.astype(int).item()
 
-def _create_Wiener_Q(n_states, alpha, tau, sigma, mu):
-    
-    Q = at.zeros((n_states, n_states))
+def _create_Wiener_Q(n_part, n_states, alpha, tau, sigma, v):
+    print(v.shape)
+    Q = at.zeros((n_part, 1, n_states, n_states)) # n_part x n_trials x n_states x n_states
     rows_ = at.arange(1,n_states)
     cols_ = at.arange(0,n_states-1)
-    Q = st.set_subtensor(Q[rows_,cols_], 1/(2*alpha)*(1-mu*at.sqrt(tau)/sigma**2))
-    Q = st.set_subtensor(Q[cols_, rows_],  1/(2*alpha)*(1+mu*at.sqrt(tau)/sigma**2))
-    Q = st.set_subtensor(Q[at.arange(n_states), at.arange(n_states)], 1-(1/alpha))
-    Q = st.set_subtensor(Q[0,1], 0)
-    Q = st.set_subtensor(Q[-1,-2], 0)
+    print(Q.shape)
+    for i in at.arange(n_part):
+      b_m = (1/(2*alpha)*(1-v[i,:]*at.sqrt(tau)/sigma**2)).reshape(-1,1)
+      b_p = (1/(2*alpha)*(1+v[i,:]*at.sqrt(tau)/sigma**2)).reshape(-1,1)
+      print(b_p.shape)
+      Q = Q.at[i,:, rows_,cols_].set(b_m)
+      Q = Q.at[i,:, cols_, rows_].set(b_p) # Because we don't need drift rates for boundary states
+      Q = Q.at[i,:, at.arange(n_states), at.arange(n_states)].set(1-(1/alpha))
+      Q = Q.at[i,:, 0,1].set(0)
+      Q = Q.at[i,:, -1,-2].set(0)
     return Q
 
-def _get_initial_state(n_states):
-   p_0 = pm.draw(pm.Dirichlet.dist(np.repeat(0.5,n_states))) # To match the Q matrix (see Diederich 2003)
-   p_0 = p_0.squeeze() #concatenate(([[0.0]], p_0, [[0.0]]), axis=1) #dirichlet.rvs(repeat(0.5,n_states))
-   #z = at.sum(p_0)
-   #while (z>1):
-      #print("^^^^^^ ",z)
-   #   p_0 = p_0 / z
-   #   z = sum(p_0)
-      #print("**** ",z)
-   #s_0 = pm.draw(pm.Multinomial.dist(n=1,p=p_0))
-   return p_0
+def get_prior(n_part, n_states, n_within_trials):
+   Z = ro.sample("start_state", dist.Dirichlet(np.repeat(0.5,n_states)), sample_shape=(n_part,1)) # To match the Q matrix (see Diederich 2003)
+   v = ro.sample("drift", dist.Normal(0,1),sample_shape=(n_part,n_within_trials))
+   return Z, v
 
-def _liklihood(t, x, v):
+def init_model(n_part, v, n_states):
    
-   alpha=1.5
-   tau = 0.01
-   theta=5
-   sigma = 1
-   Delta = alpha * np.sqrt(sigma) * np.sqrt(tau)
-   m = int(2 * np.round(theta/Delta) + 1)
-   x = np.arange(-(m-1)/2, (m-1)/2)
-
-   tm = _create_Wiener_Q(m, alpha, tau, sigma, v)
-   Q = tm[1:-1,1:-1]
-   R = tm[1:-1,[0,-1]]
    
-   Z_m = m-2
-   Z = _get_initial_state(Z_m)
-   n = (t.eval() / tau) - 1
+   #x = np.arange(-(n_states-1)/2, (n_states-1)/2)
+   
+   T_m = _create_Wiener_Q(n_part, n_states, alpha, tau, sigma, v)
+   
+   Z_m = n_states-2
+   I = at.eye(Z_m)
+   return T_m, I
 
+def _liklihood(t, x, N, T_m, Z, I):
+   
    #num1 = at.matmul(Z, at.nlinalg.matrix_power(Q, n-1))
    #num = at.matmul(num1, R)
    #den = at.matmul(at.matmul(Z, at.nlinalg.matrix_power((at.eye(m)-Q,-1))), R)
 
-   t1 = at.nlinalg.matrix_power(Q, n)
-   t2 = at.nlinalg.matrix_power(at.eye(Z_m) - Q,-1)
+   n_part, n_trials = t.shape if t is not None else (1,1)
+
+   Q = T_m[..., 1:-1,1:-1]
+   R = T_m[..., 1:-1,[0,-1]]
+
+   t1 = at.zeros((n_part, n_trials, Q.shape[-2], Q.shape[-1]))
+
+   for i in at.arange(n_part):
+      for j in at.arange(n_trials):
+         t1 = t1.at[i,j,...].set(at.linalg.matrix_power(Q[i,j], N[i,j].astype(int).item()))
+
+   t2 = at.linalg.matrix_power(I - Q, -1)
    
    num = (Z @ t1) @ R
    den = (Z @ t2) @ R
 
    Pr = num/den
-   #print(Pr.eval().shape)
-   likl = at.where(at.eq(x,0),Pr[0], Pr[1])
-   #likl = likl.sum()
+   likl = at.where(x==0,Pr[...,0,0], Pr[...,0,1]) # now working but double check
+   likl = likl.sum()
    return likl
 
+
+#if __name__ == "__main__":
+alpha=1.5
+tau = 0.01
+theta=5
+sigma = 1
+
+n_states = get_n_states(alpha, theta, tau, sigma)
+Q = _create_Wiener_Q(n_part = 1, n_states=n_states, alpha=alpha, tau=tau, sigma=sigma, v = at.asarray([0.5]).reshape(1,-1) )
+
 #%%
-if __name__ == "__main__":
-   Pr = _liklihood(at.as_tensor([[1.5]]), at.as_tensor([[1]]), at.as_tensor([0.5]))
-   print(Pr.eval().shape)
+Q = _create_Wiener_Q(n_part = 1, n_states=n_states, alpha=alpha, tau=tau, sigma=sigma, 
+                     v = at.repeat(at.asarray([[0.5]]),n_states-1).reshape(1,-1) )
+
+#%%
+
+rng = random.PRNGKey(0)
+
+#%%
+n_part = 1
+alpha=1.5 
+tau = 0.01 
+theta=5
+sigma = 1
+n_states = get_n_states(alpha, theta, tau, sigma)
+
+#res = Predictive(get_prior(n_part, n_states, n_within_trial))(rng)
+Z = dist.Dirichlet(np.repeat(0.5,n_states-2)).sample(rng, sample_shape=(1,))
+v = dist.Normal(0,1).sample(rng, sample_shape=(n_part,1))
+
+
+T_m, I = init_model(n_part, v, n_states)
+
+t, x = at.asarray([[1.5]]), at.asarray([[1]]) 
+
+N = (t / tau) - 1
+
+Pr = _liklihood(t, x, N, T_m, Z, I)
+     
    
-   with pm.Model() as model:
-      v = pm.Normal("drift", 0,1,shape=(1,))
-      likl = pm.CustomDist("rt",
-      at.as_tensor([[1]]), v,
-      logp = _liklihood,
-      observed = at.as_tensor([[1.5]])
-      )
-      az.summary(pm.sample(draws=10, tune=5))
-   
+# %%
+n_part = 2
+alpha=1.5 
+tau = 0.01 
+theta=5
+sigma = 1
+n_states = get_n_states(alpha, theta, tau, sigma)
+#res = Predictive(get_prior(n_part, n_states, n_within_trial))(rng)
+Z = dist.Dirichlet(np.repeat(0.5,n_states-2)).sample(rng, sample_shape=(1,))
+v = dist.Normal(0,1).sample(rng, sample_shape=(n_part,n_states-1))
+
+T_m, I = init_model(n_part, v, n_states)
+n_within_trial = T_m.shape[0]
+
+t, x = np.random.random((n_part,4)) + np.random.random((n_part,4)), np.random.randint(0,2, size = (n_part,4))
+
+N = (t / tau) - 1
+
+Pr = _liklihood(t, x, N, T_m, Z, I)
 # %%
